74990c00b6cdc6a6eae311876a093e86df52ddda
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-10-02 23:04:26 +0330

    develope invoice page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -26,7 +26,7 @@ export default function CartPage() {
 
   const calculateTotal = () => {
     return cartItems.reduce(
-      (total, item) => total + item.varieties[0].discounted_price * item.count,
+      (total, item) => total + Number(item.varieties[0].price) * item.count,
       0
     );
   };
@@ -59,7 +59,7 @@ export default function CartPage() {
 
   return (
     <>
-      <div className="flex flex-col ">
+      <div className="flex flex-col">
         <Image
           src={"/1.svg"}
           width={800}
@@ -67,97 +67,98 @@ export default function CartPage() {
           alt="image"
           className="!w-full relative"
         />
-      </div>
-      <div className="absolute top-[40vh] w-full !font-iranSans">
-        <div className="flex flex-col md:flex-row justify-between p-4 bg-white">
-          {/* Main section (cart items) */}
-          <main className="md:w-2/3 md:border-e w-full mb-4 md:mb-0">
-            <h2 className="text-xl font-bold mb-4">سبد خرید</h2>
-            <div className="">
-              {cartItems.length === 0 ? (
-                <p>سبد خرید شما خالی است.</p>
-              ) : (
-                cartItems.map((item) => (
-                  <div
-                    key={item.id}
-                    className="flex justify-between items-center bg-white sm:pb-4 pb-0 rounded-lg"
-                  >
-                    <div className="flex items-center gap-2">
-                      <Image
-                        src={item.image}
-                        alt={item.title}
-                        width={400}
-                        height={600}
-                        className="w-1/5 rounded-md h-fit"
-                      />
-                      <div className="flex flex-col gap-2">
-                        <h3 className="sm:text-lg font-semibold text-sm">
-                          {item.title}
-                        </h3>
-                        <p className="text-sm">
-                          قیمت:
-                          <span>
-                            {item.varieties[0].discounted_price.toLocaleString(
-                              "fa-IR"
-                            )}
-                          </span>
-                          تومان
-                        </p>
-                      </div>
-                    </div>
-                    <div className="flex items-center !w-[8rem] md:w-fit">
-                      <div className="pt-2 pt-sm-0 ps-sm-3 mx-auto mx-sm-0 text-center text-sm-end">
-                        <button
-                          onClick={() => increaseQuantity(item.id)}
-                          className="bg-gray-300 rounded-md p-2  hover:bg-gray-200 transition-all"
-                        >
-                          +
-                        </button>
-                        <span className="px-2">
-                          {item.count.toLocaleString("fa-IR")}
-                        </span>
-                        <button
-                          className={`bg-gray-300 rounded-md p-2  hover:bg-gray-200 transition-all ${
-                            item.count === 1 ? "disabled" : ""
-                          }`}
-                          onClick={() => decreaseQuantity(item.id)}
-                        >
-                          -
-                        </button>
-                        <br />
-                        <button
-                          className="flex gap-1 items-center text-red-500 bg-transparent border-none shadow-none  pe-0 text-sm md:text-md"
-                          type="button"
-                          onClick={() => removeItem(item.id)}
-                        >
-                          <RiCloseCircleLine className="ms-1" />
-                          <span className="">حذف</span>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                ))
-              )}
-            </div>
-          </main>
 
-          {/* Sidebar (total price) */}
-          <aside className="md:w-1/3 w-full bg-white p-4 rounded-lg mt-5">
-            <div className="flex justify-between items-center">
-              <span>مجموع پرداختی:</span>
-              <span className="text-xl font-bold">
-                {calculateTotal().toLocaleString("fa-IR")} تومان
-              </span>
-            </div>
-            <div className="border-t mt-5">
-              <Link
-                href={"/invoice"}
-                className="btn !w-full mt-4 py-2 bg-red-500 text-white font-semibold rounded-lg hover:bg-red-600 transition-all duration-300"
-              >
-                ثبت فاکتور و پرداخت
-              </Link>
-            </div>
-          </aside>
+        <div className="absolute top-[40vh] !w-full max-w-[1400px] !font-iranSans sm:px-8 rounded-md !overflow-hidden">
+          <div className="flex flex-col md:flex-row justify-between p-4 bg-white">
+            {/* Main section (cart items) */}
+            <main className="md:w-2/3 md:border-e w-full mb-4 md:mb-0">
+              <h2 className="text-xl font-bold mb-4">سبد خرید</h2>
+              <div className="">
+                {cartItems.length === 0 ? (
+                  <p>سبد خرید شما خالی است.</p>
+                ) : (
+                  cartItems.map((item) => (
+                    <div
+                      key={item.id}
+                      className="flex justify-between items-center bg-white sm:pb-4 pb-0 rounded-lg"
+                    >
+                      <div className="flex items-center gap-2">
+                        <Image
+                          src={item.image}
+                          alt={item.title}
+                          width={400}
+                          height={600}
+                          className="w-1/5 rounded-md h-fit"
+                        />
+                        <div className="flex flex-col gap-2">
+                          <h3 className="sm:text-lg font-semibold text-sm">
+                            {item.title}
+                          </h3>
+                          <p className="text-sm">
+                            قیمت:
+                            <span>
+                              {Number(item.varieties[0].price).toLocaleString(
+                                "fa-IR"
+                              )}
+                            </span>
+                            تومان
+                          </p>
+                        </div>
+                      </div>
+                      <div className="flex items-center !w-[8rem] md:w-fit">
+                        <div className="pt-2 pt-sm-0 ps-sm-3 mx-auto mx-sm-0 text-center text-sm-end">
+                          <button
+                            onClick={() => increaseQuantity(item.id)}
+                            className="bg-gray-300 rounded-md p-2  hover:bg-gray-200 transition-all"
+                          >
+                            +
+                          </button>
+                          <span className="px-2">
+                            {item.count.toLocaleString("fa-IR")}
+                          </span>
+                          <button
+                            className={`bg-gray-300 rounded-md p-2  hover:bg-gray-200 transition-all ${
+                              item.count === 1 ? "disabled" : ""
+                            }`}
+                            onClick={() => decreaseQuantity(item.id)}
+                          >
+                            -
+                          </button>
+                          <br />
+                          <button
+                            className="flex gap-1 items-center text-red-500 bg-transparent border-none shadow-none  pe-0 text-sm md:text-md"
+                            type="button"
+                            onClick={() => removeItem(item.id)}
+                          >
+                            <RiCloseCircleLine className="ms-1" />
+                            <span className="">حذف</span>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                )}
+              </div>
+            </main>
+
+            {/* Sidebar (total price) */}
+            <aside className="md:w-1/3 w-full bg-white p-4 rounded-lg mt-5">
+              <div className="flex justify-between items-center">
+                <span>مجموع پرداختی:</span>
+                <span className="text-xl font-bold">
+                  {calculateTotal().toLocaleString("fa-IR")} تومان
+                </span>
+              </div>
+              <div className="border-t mt-5">
+                <Link
+                  href={"/invoice"}
+                  className="btn !w-full mt-4 py-2 bg-red-500 text-white font-semibold rounded-lg hover:bg-red-600 transition-all duration-300"
+                >
+                  ثبت فاکتور و پرداخت
+                </Link>
+              </div>
+            </aside>
+          </div>
         </div>
       </div>
     </>
--- src/app/invoice/page.tsx
+++ src/app/invoice/page.tsx
@@ -1,6 +1,43 @@
-import React from "react";
+"use client";
+import { CartContext } from "@/context/CartContext";
+import React, { useContext, useEffect, useState } from "react";
 
-export default function page() {
+// تعریف نوع برای محصول
+interface Product {
+  id: number;
+  title: string;
+  varieties: {
+    price: number;
+  }[];
+  count: number;
+}
+
+export default function Page() {
+  const [cartItems, setCartItems] = useState<Product[]>([]);
+  const cartContext = useContext(CartContext);
+
+  // گرفتن اطلاعات از LocalStorage
+  useEffect(() => {
+    const savedCart = localStorage.getItem("cart");
+    if (savedCart) {
+      setCartItems(JSON.parse(savedCart));
+    }
+    const calculateTotal = () => {
+      return cartItems.reduce(
+        (total, item) => total + item.varieties[0].price * item.count,
+        0
+      );
+    };
+  }, []);
+  const calculateTotal = () => {
+    if (cartItems) {
+      return cartItems.reduce(
+        (total, item) => total + item.varieties[0].price * item.count,
+        0
+      );
+    }
+  };
+
   return (
     <div className="p-8 font-iranSans">
       <div>
@@ -15,32 +52,37 @@ export default function page() {
               <tr>
                 <th>تعداد</th>
                 <th>نام آیتم</th>
-                <th>قیمت آیتم</th>
-                <th>مجموع</th>
+                <th>قیمت آیتم (تومان)</th>
+                <th>مجموع (تومان)</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
-              {/* row 1 */}
-              <tr className="hover">
-                <th>1</th>
-                <td>زرشک پلو</td>
-                <td>250000</td>
-                <td>250000</td>
-              </tr>
-              {/* row 2 */}
-              <tr className="hover">
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>Desktop Support Technician</td>
-                <td>Purple</td>
-              </tr>
-              {/* row 3 */}
-              <tr className="hover">
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Red</td>
-              </tr>
+              {/* تکرار اطلاعات محصولات */}
+              {cartItems.map((item) => (
+                <tr key={item.id} className="hover">
+                  <th>{item.count}</th>
+                  <td>{item.title}</td>
+                  <td>
+                    {Number(item.varieties[0].price).toLocaleString("fa-IR")}
+                  </td>
+                  <td>
+                    {Number(
+                      item.count * item.varieties[0].price
+                    ).toLocaleString("fa-IR")}
+                  </td>
+                </tr>
+              ))}
+              <td></td>
+              <td>
+                {" "}
+                {(cartContext?.selectedTables && (
+                  <span className="px-2 py-1 bg-gray-100 rounded-md">{`میز ${cartContext.selectedTables} نفره`}</span>
+                )) ||
+                  "انتخاب نشده"}
+              </td>
+              <td></td>
+              <td> {calculateTotal()?.toLocaleString("fa-IR")} تومان</td>
             </tbody>
           </table>
         </div>
